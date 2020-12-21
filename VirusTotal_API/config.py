d355bc30e588b04e36bb4b86023495347a4696f1
PC <pranavc10@hotmail.com>
2020-12-21 01:48:41 -0800

    Removed config

--- VirusTotal_API/config.py
+++
@@ -1,4 +0,0 @@
-Meta_Defender_API_Key =  "a627fd093f3890c31b13d85b8955c644"
-Virus_Total_API_Key = "2482ba034d4adb1f76c247737491bcc3672d1884aafca71810a82e787d16e553"
-URL_Scan_IO_API_Key = "5ebb7b3c-3ec1-44c6-8aaa-ecaac5b81951"
-
