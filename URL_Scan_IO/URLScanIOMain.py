5f875aee742b37b33ccf512c1931033cab643ee6
PC <pranavc10@hotmail.com>
2020-12-21 05:37:05 -0800

    Finalised urlscanio

--- URL_Scan_IO/URLScanIOMain.py
+++ URL_Scan_IO/URLScanIOMain.py
@@ -16,15 +16,33 @@ def scanURL():
 	data = {"url": url, "visibility": "public"}
 	
 	response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
-	print(response)
 
-	time.sleep(20)						# Allow time for scanning
-	
-	resultUrl = response.json().get("api")		# Get result url
-	print(resultUrl)
+	start = str(response).find("[") + len("[")
+	end = str(response).find("]")
+	respCode = str(response)[start:end]
 
-	urlResp = requests.get(resultUrl)	# Get JSON data from result link
-	print(urlResp.json().get(data))
+	if(respCode != "200"):
+		print("Error in URL, it may be blacklisted or entered incorrectly.")
+		exit()
 
+	try:
 
+		print("Processing request... (May take upto 20 seconds)\n")
+
+		time.sleep(20)						# Allow time for scanning
+		
+		resultUrl = response.json().get("api")		# Get result url
+
+		urlResp = requests.get(resultUrl)	# Get JSON data from result link
+		print("URL: " + url)
+		print("IP: " + urlResp.json().get('page').get('ip'))
+		print("Server: " + urlResp.json().get('page').get('server'))
+		print("Ads Detected: " + str(urlResp.json().get('stats').get('adBlocked')))
+		print("Malicious: " + str(urlResp.json().get('verdicts').get('overall').get('malicious')))
+
+		print("\n\nFor more detailed results visit: " + resultUrl + "\n")
+
+	except:
+		print("Error something went wrong!")
+		
 scanURL()
