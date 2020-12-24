9bf24cae0cc3ec1a6d9a7618087dd4f9f2646a91
lolmokha <mkhan0138@gmail.com>
2020-12-24 07:40:00 +0000

    VirusTotal API now gives user-friendly output instead of json

--- VirusTotal_API/VirusTotal_API_URL.py
+++ VirusTotal_API/VirusTotal_API_URL.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import sys
+import json
 sys.path.append("..")
 from config import Virus_Total_API_key
 
@@ -16,10 +17,22 @@ scanResp = requests.post(scanUrl, data=scanParams)						# scan request
 scanID = scanResp.json().get('scan_id')									# getting scan ID
 
 print("Processing URL...")			
-time.sleep(10)															# allowing for processing time
+#time.sleep(10)															# allowing for processing time
 
 reportParams = {"apikey":Virus_Total_API_key(), 'resource' : scanID}
 reportResp = requests.get(reportUrl, params=reportParams)				# getting report of scan ID
 
-
-print(reportResp.json())				
+x = reportResp.json()
+print("Your scan ID is " + x["scan_id"] + "\n") 
+if (x["positives"] == 0):
+    print("This site is 100% safe\n")	
+else:
+    print("Our scans picked up some issues with this site. Proceed with caution.More info:")
+    z = x["scans"]
+    for obj in z:
+        if (z[obj]["detected"] != False ):
+            print (obj + " has detected an error")
+            print(z[obj])
+print("\n")
+print("To get more information on the scan,paste this link in your browser\n")
+print(x["permalink"])
