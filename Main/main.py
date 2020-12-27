b9016831e42fa12d7552225fc20ac5ba40be255c
Vicky1224 <55647590+Vickykarkera727@users.noreply.github.com>
2020-12-27 21:23:11 +0400

    Update main.py

--- Main/main.py
+++ Main/main.py
@@ -1,8 +1,12 @@
+import os
 from subprocess import call
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) #Project root
+CONFIG_PATH = os.path.join(ROOT_DIR,'URL_Scan_IO\URLScanIOMain.py')
 
 class CallPy(object):
 
-  def __init__(self,path='../../Virus-Scanner/URL_Scan_IO/URLScanIOMain.py'):
+  def __init__(self,path=CONFIG_PATH):
      self.path=path
 
   def call_python_file(self):
