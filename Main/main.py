e308a4a4898c8e5f31175a8cdc56301e425192db
Sanjana <sanjanasundar2002@gmail.com>
2020-12-28 20:42:39 +0400

    last commit

--- Main/main.py
+++ Main/main.py
@@ -1,30 +1,33 @@
 import sys
 import os
-from subprocess import call
-
-# Makes main have a global view of the root directory 
 
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
-class CallPy(object):
 
-    choice = int(input("Enter your choice 3 or 4: "))
+#Option menu
+print (
+    "1.Meta defender" +"\n" +"2.Url scan "+"\n"+"3.virus total file"+"\n"+"4.virus total url"
+)
 
-    if choice == 3:
-       def __init__(self,path='..\VirusTotal_API\VirusTotal_API_File.py'): #../ added to go to parent directory
-           self.path=path
-    elif choice ==4:
-       def __init__(self,path='..\VirusTotal_API\VirusTotal_API_URL.py'): #../ added to go to parent directory
-           self.path=path
-     
-    def call_python_file(self):
-       call(["python3","{}".format(self.path)])
-	
-if __name__=="__main__":
-	c=CallPy()
-	c.call_python_file()
+choice =int(input("Enter the choice "))
+
+#redircting to required api function or .py file
+
+if choice==1 :
+   from Meta_Defender.MetaDefenderMain import scanFile
+   scanFile()
+elif choice==2 :
+    from URL_Scan_IO.URLScanIOMain import scanURL
+    scanURL()
+
+elif choice==3:
+    from VirusTotal_API.VirusTotal_API_File import ScanFile
+    ScanFile()
+elif choice==4:
+    from VirusTotal_API.VirusTotal_API_URL import scanURL
+    scanURL()
 
 
  
