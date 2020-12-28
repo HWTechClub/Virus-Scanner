498497936891ff8cc065b6de0cea338f55cee8f6
Vicky1224 <55647590+Vickykarkera727@users.noreply.github.com>
2020-12-28 13:43:31 +0400

    Update main.py

--- Main/main.py
+++ Main/main.py
@@ -1,18 +1,30 @@
+import sys
 import os
 from subprocess import call
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) #Project root
-CONFIG_PATH = os.path.join(ROOT_DIR,'URL_Scan_IO\URLScanIOMain.py')
+# Makes main have a global view of the root directory 
+
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 class CallPy(object):
 
-  def __init__(self,path=CONFIG_PATH):
-     self.path=path
+    choice = int(input("Enter your choice 3 or 4: "))
 
-  def call_python_file(self):
-     call(["python3","{}".format(self.path)])
+    if choice == 3:
+       def __init__(self,path='..\VirusTotal_API\VirusTotal_API_File.py'): #../ added to go to parent directory
+           self.path=path
+    elif choice ==4:
+       def __init__(self,path='..\VirusTotal_API\VirusTotal_API_URL.py'): #../ added to go to parent directory
+           self.path=path
+     
+    def call_python_file(self):
+       call(["python3","{}".format(self.path)])
 	
 if __name__=="__main__":
 	c=CallPy()
 	c.call_python_file()
+
+
  
