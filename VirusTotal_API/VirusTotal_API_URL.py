dc9b38407a5c926d522c1b3df720aac5223e0a67
lolmokha <mkhan0138@gmail.com>
2021-01-06 10:32:45 +0000

    added try except statement for file scanners(VTF and M)

--- VirusTotal_API/VirusTotal_API_URL.py
+++ VirusTotal_API/VirusTotal_API_URL.py
@@ -11,32 +11,33 @@ import json
 Parameters (<url to scan>,<virus total api key>)
 '''
 def ScanURL(urlLink, Virus_Total_API_key):
+    try:
+        scanUrl = 'https://www.virustotal.com/vtapi/v2/url/scan'				# url to request scan from
+        reportUrl = 'https://www.virustotal.com/vtapi/v2/url/report'			# url to request report from
 
-    scanUrl = 'https://www.virustotal.com/vtapi/v2/url/scan'				# url to request scan from
-    reportUrl = 'https://www.virustotal.com/vtapi/v2/url/report'			# url to request report from
+        scanParams = {"apikey":Virus_Total_API_key, 'url' : urlLink}			# sending url to be scanned
+        scanResp = requests.post(scanUrl, data=scanParams)						# scan request
 
-    scanParams = {"apikey":Virus_Total_API_key, 'url' : urlLink}			# sending url to be scanned
-    scanResp = requests.post(scanUrl, data=scanParams)						# scan request
+        scanID = scanResp.json().get('scan_id')									# getting scan ID
 
-    scanID = scanResp.json().get('scan_id')									# getting scan ID
+        print("\nProcessing URL...")			
 
-    print("\nProcessing URL...")			
+        reportParams = {"apikey":Virus_Total_API_key, 'resource' : scanID}
+        reportResp = requests.get(reportUrl, params=reportParams)				# getting report of scan ID
 
-    reportParams = {"apikey":Virus_Total_API_key, 'resource' : scanID}
-    reportResp = requests.get(reportUrl, params=reportParams)				# getting report of scan ID
-
-    x = reportResp.json()
-    print("Your scan ID is " + x["scan_id"] + "\n") 
-    if (x["positives"] == 0):
-        print("This site is 100% safe\n")	
-    else:
-        print("Our scans picked up some issues with this site. Proceed with caution.More info:")
-        z = x["scans"]
-        for obj in z:
-            if (z[obj]["detected"] != False ):
-                print (obj + " has detected that it may be a " + z[obj]["result"])
-                print(z[obj])
-    print("\n")
-    print("To get more information on the scan,paste this link in your browser\n")
-    print(x["permalink"])
-
+        x = reportResp.json()
+        print("Your scan ID is " + x["scan_id"] + "\n") 
+        if (x["positives"] == 0):
+            print("This site is 100% safe\n")	
+        else:
+            print("Our scans picked up some issues with this site. Proceed with caution.More info:")
+            z = x["scans"]
+            for obj in z:
+                if (z[obj]["detected"] != False ):
+                    print (obj + " has detected that it may be a " + z[obj]["result"])
+                    print(z[obj])
+        print("\n")
+        print("To get more information on the scan,paste this link in your browser\n")
+        print(x["permalink"])
+    except:
+        print("We ran into some errors.Please try again")
