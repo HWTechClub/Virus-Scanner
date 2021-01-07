9a5b885f1dc4c6ea6c230e34e686c6b9ea4f0aff
mk2008 <mk2008@hw.ac.uk>
2021-01-07 18:55:22 +0400

    Edits to main.py to make it work with the new changes

--- Main/main.py
+++ Main/main.py
@@ -9,10 +9,11 @@ def mainfile():
 	PACKAGE_PARENT = '..'
 	SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 	sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
-
-	if (os.path.isfile("../config.yaml")!= True):
+	sys.path.append('../')
+	import config
+	if (os.path.isfile("config.yaml")!= True):
 		print("No config file")
-		f = open("../config.yaml","w+")
+		f = open("config.yaml","w+")
 		metakey = input("Enter API key for MetaDefender: ")
 		virusscankey = input ("Enter API key for VirusTotal: ")
 		f.write("Meta_Defender_API_key: " + metakey + "\n")
@@ -38,17 +39,20 @@ def mainfile():
 
 		if choice==1 :
 			from Meta_Defender.MetaDefenderMain import scanFile
-			scanFile()
+			filepath = input("Please enter the path of the file to scan: ")
+			scanFile(filepath,config.Meta_Defender_API_key())
 			continue
         
 		elif choice==2 :
 			from VirusTotal_API.VirusTotal_API_File import ScanFile
-			ScanFile()
+			filepath = input("Please enter the path of the file to scan: ")
+			ScanFile(filepath,config.Virus_Total_API_key)
 			continue
 
 		elif choice==3:
-			from VirusTotal_API.VirusTotal_API_URL import scanURL
-			scanURL()
+			from VirusTotal_API.VirusTotal_API_URL import ScanURL
+			url = input("Please enter the URL of the site to scan: ")
+			ScanURL(url,config.Virus_Total_API_key)
 			continue
 
 		elif choice==4:
