3da4c1003a099532d75831ed7ca8efab87ad056e
PC-02 <pranavc10@hotmail.com>
2021-06-23 20:47:16 +0400

    adding setup file for pip

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ with open("README.md", "r") as fh:
 setuptools.setup(
      name='vscan',
      version='0.1',
-     scripts=['vscan','config.py',
      author="HWU Sec Team",
      author_email="pranavc10@hotmail.com",
      description="A cli tool that uses APIs to scan malicious files & urls",
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/HWTechClub/Virus-Scanner",
-     packages=setuptools.find_packages(),
+     packages=["virus_scanner","virus_scanner/Main","virus_scanner/Meta_Defender","virus_scanner/VirusTotal_API"],
+     scripts=["vscan"],
      install_requires=['pyyaml',
-			'tkintertable',
-			'pyfiglet',
-			'pypi-cli',
-			'PyInquirer'
+            'tkintertable',
+            'pyfiglet',
+            'pypi-cli',
+            'PyInquirer'
                        ],
      classifiers=[
          "Programming Language :: Python :: 3",
