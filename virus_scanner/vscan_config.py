d75f7c1708604e7d06455ff995f94fe51e4cd691
PC-02 <pranavc10@hotmail.com>
2021-06-23 20:54:23 +0400

    added init

--- virus_scanner/vscan_config.py
+++ virus_scanner/vscan_config.py
@@ -3,12 +3,22 @@
 # demonstrates to:
 #   - returns data from config.yaml file 
 # -------------------------------------------------------------------------------------------------------------------------------------
+
+import platform
+import os
+import virus_scanner as vs
 import yaml
+
+# Config file
+if platform.system() == "Windows":
+    config_path = os.path.dirname(vs.__file__) + "\\config.yaml"
+else:
+    config_path = os.path.dirname(vs.__file__) + "/config.yaml"
 
 # ------------------------------------------------- Reads config.yaml file ------------------------------------------------------------
 
 def read_config():
-    with open('config.yaml') as f:
+    with open(config_path) as f:
       return yaml.load(f, Loader=yaml.FullLoader)
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------
@@ -19,7 +29,7 @@ def Meta_Defender_API_key():
     try:
        return data["Meta_Defender_API_key"]
     except:
-        print("Error Meta Defender API key not found/invalid")
+        print("Error Meta Defender API key not found.")
         exit()
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------
@@ -30,7 +40,7 @@ def Virus_Total_API_key():
     try:
         return data["Virus_Total_API_key"]
     except:
-        print("Error Virus Total API key not found/invalid")
+        print("Error Virus Total API key not found.")
         exit()
    
 
